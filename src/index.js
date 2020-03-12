8f6aaf52916b722d2f071ebaf8a1d19fd7cc6c48
Rafael Zerpa <zrapar@gmail.com>
2020-03-12 10:23:25 -0400

    pollifyll

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 // Support for Internet Explorer 11 requires polyfills.
 // For to support Internet Explorer 11, install react-app-polyfill,
 // https://github.com/facebook/create-react-app/tree/master/packages/react-app-polyfill
-// import 'react-app-polyfill/ie11';
-// import 'react-app-polyfill/stable';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import 'typeface-muli';
 import './react-table-defaults';
 import './react-chartjs-2-defaults';
@@ -13,10 +13,7 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from 'app/App';
 
-ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
