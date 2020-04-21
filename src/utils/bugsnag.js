49811e9bdccc40ab276735b810a70176b93fce90
Rafael Zerpa <zrapar@gmail.com>
2020-04-21 04:59:44 -0400

    update package

--- src/utils/bugsnag.js
+++ src/utils/bugsnag.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Bugsnag from '@bugsnag/js';
 import BugsnagPluginReact from '@bugsnag/plugin-react';
-const appVersion = require('../../package.json').version;
 
 Bugsnag.start({
 	apiKey       : process.env.REACT_APP_Bugsnag_Key,
 	plugins      : [ new BugsnagPluginReact(React) ],
 	appType      : 'client',
-	releaseStage : process.env.NODE_ENV,
-	appVersion
+	releaseStage : process.env.NODE_ENV
 });
 
 export const BugsnagReporter = Bugsnag;
