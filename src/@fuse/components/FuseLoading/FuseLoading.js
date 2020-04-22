1a1695f8c238c0bfd706a5549024b3331387fdbc
Rafael Zerpa <zrapar@gmail.com>
2020-04-21 23:41:18 -0400

    new message on loading

--- src/@fuse/components/FuseLoading/FuseLoading.js
+++ src/@fuse/components/FuseLoading/FuseLoading.js
@@ -1,35 +1,35 @@
-import React, {useState} from 'react';
-import {Typography, LinearProgress} from '@material-ui/core';
-import {useTimeout} from '@fuse/hooks';
+import React, { useState } from 'react';
+import { Typography, LinearProgress } from '@material-ui/core';
+import { useTimeout } from '@fuse/hooks';
 import PropTypes from 'prop-types';
 
-function FuseLoading(props)
-{
-    const [showLoading, setShowLoading] = useState(!props.delay);
+function FuseLoading(props) {
+	const [ showLoading, setShowLoading ] = useState(!props.delay);
 
-    useTimeout(() => {
-        setShowLoading(true);
-    }, props.delay);
+	useTimeout(() => {
+		setShowLoading(true);
+	}, props.delay);
 
-    if ( !showLoading )
-    {
-        return null;
-    }
+	if (!showLoading) {
+		return null;
+	}
 
-    return (
-        <div className="flex flex-1 flex-col items-center justify-center">
-            <Typography className="text-20 mb-16" color="textSecondary">Loading...</Typography>
-            <LinearProgress className="w-xs" color="secondary"/>
-        </div>
-    );
+	return (
+		<div className='flex flex-1 flex-col items-center justify-center'>
+			<Typography className='text-20 mb-16' color='textSecondary'>
+				Cargando, por favor espere...
+			</Typography>
+			<LinearProgress className='w-xs' color='secondary' />
+		</div>
+	);
 }
 
 FuseLoading.propTypes = {
-    delay: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
+	delay : PropTypes.oneOfType([ PropTypes.number, PropTypes.bool ])
 };
 
 FuseLoading.defaultProps = {
-    delay: false
+	delay : false
 };
 
 export default FuseLoading;
